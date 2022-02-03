076e3a16de87705c81aef36ebf223471872f40d3
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2022-02-03 08:32:51 +0600

    Add files via upload
    // JavaScript Data Types

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+// JavaScript Data Types
+/*commen= 1 */
+Data:2,Primitive,NonPrimitive
+// A:Primitive:String,Number,Boolean,undefined,BigInt "10n",Symbol,null;
+
+let phone = 'OnePlus'; String
+let phone = '7+8+ OnePlus'; 
+let phone = 'OnePlus'+7+8; 
+let phone = "My name is 'Mamun Khan'";
+let phone = 'My name is "Mamun Khan"';
+let model = 7;Number
+let phone = 7.7;
+/*commen= 2 */
+let phone = 8;
+let mobile = 8;
+document.getElementById('demo').innerHTML = (phone ==mobile); true & false=Boolean;
+/*commen= 3 */
+let phone = ['samsung','Apple','oneplus'];(Array);
+phone[3] = 'Nokia';Add Kora jaba,OR:phone.push('Mamun');  Remov =1,names.shift,Remov =2,names.pop;
+document.getElementById('demo').innerHTML = [];Number Thea Akti print Koragai;
+console.log(phone);
+/*commen= 4(object) */
+// NonPrimitive:Object,function, Array;
+let phone = {
+    model:'ihone 11',
+    Storage:'123GB',
+    color:'black',
+}
+phone.Storage = '325GB';Bahere Thaka chang Koragai;
+
+document.getElementById('demo').innerHTML =phone.Storage;
+console.log(phone);
